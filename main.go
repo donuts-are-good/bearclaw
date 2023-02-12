1ef5944a4219e4c661c40eb90509a75c0d4aab74
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-02-12 04:34:31 -0600

    adding comments to the code

--- main.go
+++ main.go
@@ -15,55 +15,91 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+// paths
 var (
-	inFolder       = "./markdown"
-	outFolder      = "./output"
-	templateFolder = "./templates"
+	inFolder       = "./markdown"  // your markdown articles go in here
+	outFolder      = "./output"    // your rendered html will end up here
+	templateFolder = "./templates" // your header and footer go here
+
 )
 
+// init runs before main()
 func init() {
+
+	// we are making a list of folders here to check for the presence of
+	// if they don't exist, we create them
 	foldersToCreate := []string{inFolder, outFolder, templateFolder}
 	createFoldersErr := createFolders(foldersToCreate)
 	if createFoldersErr != nil {
 		log.Println(createFoldersErr)
 	}
+
 }
 
 func main() {
+
+	// check to see if the user ran with --watch
 	watchFlag := flag.Bool("watch", false, "watch the current directory for changes")
 	flag.Parse()
 
+	// if they did...
 	if *watchFlag {
+
+		// make a list of folders to keep an eye on
 		foldersToWatch := []string{templateFolder, inFolder}
+
+		// the process to watch it goes in a goroutine
 		go watchFoldersForChanges(foldersToWatch)
+
+		// give the user some type of confirmation
 		fmt.Println("Waiting for changes: ", foldersToWatch)
+
+		// select {} is a blocking operation that keeps
+		// the program from closing
 		select {}
 	}
 
+	// now, if nothing has gone wrong, we process the html
 	markdownToHTML(inFolder, outFolder, templateFolder)
 	createPostList(inFolder, outFolder, templateFolder)
+
 }
 
 func watchFoldersForChanges(folders []string) {
+
+	// range through the watched files
 	for _, folder := range folders {
+
+		// create a new watcher for each watched folder
 		watcher, err := fsnotify.NewWatcher()
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// don't forget to close it
 		defer watcher.Close()
 
+		// make a channel for goroutine messaging
 		done := make(chan bool)
 		go func() {
+
+			// for { ev ... ver }
 			for {
 				select {
 				case event := <-watcher.Events:
 					if event.Op&fsnotify.Write == fsnotify.Write {
+
+						// if there's an event, remark and rebuild
 						log.Println("modified:", event.Name, " - rebuilding files..")
 						markdownToHTML(inFolder, outFolder, templateFolder)
 						createPostList(inFolder, outFolder, templateFolder)
+
 					}
+
+					// if we get an error instead of event, announce it
 				case err := <-watcher.Errors:
 					log.Println("error:", err)
+
 				}
 			}
 		}()
@@ -76,6 +112,7 @@ func watchFoldersForChanges(folders []string) {
 	}
 }
 
+// createFolders takes a list of folders and checks for them to exist, and creates them if they don't exist.
 func createFolders(folders []string) error {
 	for _, folder := range folders {
 		if _, err := os.Stat(folder); os.IsNotExist(err) {
@@ -88,43 +125,87 @@ func createFolders(folders []string) error {
 	return nil
 }
 
+// markdownToHTML converts markdown documents to HTML
 func markdownToHTML(inFolder, outFolder, templateFolder string) {
 	files, _ := os.ReadDir(inFolder)
 
 	for _, file := range files {
+
+		// only markdown files
 		if filepath.Ext(file.Name()) == ".md" {
+
+			// open the selected markdown file
 			markdownFile, _ := os.Open(inFolder + "/" + file.Name())
+
+			// don't forget to close it when done
 			defer markdownFile.Close()
+
+			// create the html file
 			htmlFile, _ := os.Create(outFolder + "/" + file.Name() + ".html")
 			defer htmlFile.Close()
+
+			// read the md
 			reader := bufio.NewReader(markdownFile)
 			markdown, _ := io.ReadAll(reader)
+
+			// send the md to blackfriday
 			html := blackfriday.MarkdownCommon(markdown)
+
+			// read in our templates
 			header, _ := os.ReadFile(templateFolder + "/header.html")
 			footer, _ := os.ReadFile(templateFolder + "/footer.html")
+
+			// put it all together in order
 			fmt.Fprintln(htmlFile, string(header)+strings.TrimSpace(string(html))+string(footer))
 		}
 	}
 }
 
+// createPostList creates the page that has a list of all of your posts
 func createPostList(inFolder, outFolder, templateFolder string) {
+
+	// read the files in the directory
 	files, _ := os.ReadDir(inFolder)
+
+	// sort them by mod time
 	sort.Slice(files, func(i, j int) bool {
 		fi, _ := os.Stat(inFolder + "/" + files[i].Name())
 		fj, _ := os.Stat(inFolder + "/" + files[j].Name())
 		return fi.ModTime().After(fj.ModTime())
 	})
+
+	// unordered list
 	postList := "<ul>"
+
+	// for all files...
 	for _, file := range files {
+
+		// if it is a markdown file...
 		if filepath.Ext(file.Name()) == ".md" {
+
+			// get the filename/title
 			title := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
+
+			// put it on the list with the html
 			postList += "<li><a href='" + file.Name() + ".html'>" + title + "</a></li>"
+
 		}
+
 	}
+
+	// end the list
 	postList += "</ul>"
+
+	// create the posts file
 	htmlFile, _ := os.Create(outFolder + "/posts.html")
+
+	// don't forget to close it
 	defer htmlFile.Close()
+
+	// read the header/footer templates
 	header, _ := os.ReadFile(templateFolder + "/header.html")
 	footer, _ := os.ReadFile(templateFolder + "/footer.html")
+
+	// combine them
 	fmt.Fprintln(htmlFile, string(header)+postList+string(footer))
 }
